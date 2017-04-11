7e2ea18b52ef057db5a9ec946198730e9a1e312c
Sera Yang <sera.yang@berkeley.edu>
2017-04-11 15:34:59 -0700

    projects has been formated, education has been removed, intro has been reformated

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-    $('#link-edu').click(function() {
-        $('body').animate({scrollTop: $('#education').offset().top-120});
+    $('#link-sera').click(function() {
+        $('body').animate({scrollTop: $('#sera').offset().top-80});
         return false;
     });
 
     $('#link-proj').click(function() {
-        $('body').animate({scrollTop: $('#projects').offset().top-120});
+        $('body').animate({scrollTop: $('#projects').offset().top-80});
         return false;
     });
 
     $('#link-work').click(function() {
-        $('body').animate({scrollTop: $('#work').offset().top-120});
+        $('body').animate({scrollTop: $('#work').offset().top-80});
         return false;
     });
 
     $('#link-skills').click(function() {
-        $('body').animate({scrollTop: $('#skills').offset().top-120});
+        $('body').animate({scrollTop: $('#skills').offset().top-80});
         return false;
     });
 })
